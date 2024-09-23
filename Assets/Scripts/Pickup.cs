d151d07d3d86f2018f808f125fdec2347a41fa97
Chunch <NWAColec7275@gmail.com>
2024-09-22 19:59:19 -0700

    Cleanup & added basic plan

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -6,17 +6,13 @@ public class Pickup : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            // Get the playerstats component from the player GameObject
             playerstats stats = other.GetComponent<playerstats>();
             if (stats != null)
             {
-                // Increase the HealItems float in playerstats
                 stats.HealItems++;
 
-                // Log the current number of heal items
                 GameManager.LogHealItems();
 
-                // Destroy the pickup object
                 Destroy(gameObject);
             }
         }
